19c73e3afec0ea751a7d5e68da1cab4e87f2efd7
Ketizsingh <Gurpreettechnologies@gmail.com>
2020-11-13 18:45:30 -0500

    function focus and blue added

--- js/chapter09-project01.js
+++ js/chapter09-project01.js
@@ -2,6 +2,19 @@ window.addEventListener("load", function () {
     document.querySelectorAll('.hilightable').forEach(item => {
 
     	var hilightable = document.querySelector('.hilightable');
+
+         item.addEventListener('focus', event => {
+            item.classList.remove('hilightable');
+            item.classList.add('highlight');
+        });
+
+
+        item.addEventListener('blur', event => {
+            item.classList.remove('highlight');
+            item.classList.add('hilightable');
+        });
+
+        
     	
     });
 });
