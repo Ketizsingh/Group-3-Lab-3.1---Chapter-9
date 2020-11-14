e4044be228b72dc8ac2847c38dcae7bb30f790cb
Mohit Rawal <mohitrawal3131@gmail.com>
2020-11-13 19:07:15 -0500

    event handler added

--- ch09-project2-start-master/js/chapter09-project02.js
+++ ch09-project2-start-master/js/chapter09-project02.js
@@ -5,4 +5,11 @@ window.addEventListener("load", function () {
         document.querySelector('#featured > figcaption').style.transition = 'opacity 1s';
 
     });
+    
+    document.getElementById('featured').addEventListener("mouseout", function( event ) {
+       
+        document.querySelector('#featured > figcaption').style.opacity='0%';
+        document.querySelector('#featured > figcaption').style.transition='opacity 1s';
+
+    });
 });
