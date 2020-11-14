cdb55be7a624bc620fb0a5f8cfc7be8207d8a670
Ketizsingh <Gurpreettechnologies@gmail.com>
2020-11-13 19:08:16 -0500

    image replacer function

--- ch09-project2-start-master/js/chapter09-project02.js
+++ ch09-project2-start-master/js/chapter09-project02.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function () {
+	
     document.getElementById('featured').addEventListener("mouseover", function (event) {
 
         document.querySelector('#featured > figcaption').style.opacity = '80%';
@@ -12,4 +13,21 @@ window.addEventListener("load", function () {
         document.querySelector('#featured > figcaption').style.transition='opacity 1s';
 
     });
+
+
+    document.getElementById('thumbnails').onclick = function(event)
+    {
+        if(event.target.tagName == 'IMG')
+        {
+            let title = event.target.title;
+            let src = event.target.src.split(/(\\|\/)/g).pop();
+            let alt = event.target.alt;
+            
+            document.querySelector('#featured > img').src='images/medium/'+src;
+            document.querySelector('#featured > img').title=title;
+            document.querySelector('#featured > figcaption').innerHTML = alt;
+           
+          
+        }
+    }
 });
