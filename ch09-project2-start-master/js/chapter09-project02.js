21b85a281fefb58df936c6b3a1e37c9941f697bb
Nikhil Rawal <rawal.nikhil29@gmail.com>
2020-11-13 19:05:27 -0500

    event handler added

--- ch09-project2-start-master/js/chapter09-project02.js
+++ ch09-project2-start-master/js/chapter09-project02.js
@@ -1,3 +1,8 @@
-window.addEventListener("load", function(){
-    
+window.addEventListener("load", function () {
+    document.getElementById('featured').addEventListener("mouseover", function (event) {
+
+        document.querySelector('#featured > figcaption').style.opacity = '80%';
+        document.querySelector('#featured > figcaption').style.transition = 'opacity 1s';
+
+    });
 });
